1184eccd53fbf4ba042f83792341a08d38f1afae
Victor Sokoluik <vic.sokoliuk@gmail.com>
2021-05-30 23:53:25 +0300

    a little bug)

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 				$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 				
 				if ( has_custom_logo() ) {
-					echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+					echo '<a href="/"><img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '"></a>';
 				} else {
 					echo '<h1>' . get_bloginfo('name') . '</h1>';
 				}
